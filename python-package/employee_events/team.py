912ee20eeff3c60e107925d801938d7291bb2374
takumi.matsubara <takumi.matsubara@accenture.com>
2025-07-28 15:30:17 +0900

    developing

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,37 +1,49 @@
 # Import the QueryBase class
 # YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies for sql execution
 #### YOUR CODE HERE
+from .sql_execution import query
+
 
 # Create a subclass of QueryBase
 # called  `Team`
 #### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
     #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-        
+    @query
+    def names(self):
+
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
         #### YOUR CODE HERE
-    
+        return """
+            SELECT team_name,
+            team_id
+            FROM team
+            """
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
+    # TODO 返り値
+    @query
+    def username(self, id):
 
         # Query 6
         # Write an SQL query
@@ -40,7 +52,11 @@
         # to only return the team name related to
         # the ID argument
         #### YOUR CODE HERE
-
+        return f"""
+            SELECT team_name
+            FROM team
+            WHERE team_id = {id}
+            """
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -51,8 +67,7 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        query = f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
@@ -64,3 +79,4 @@
                     GROUP BY employee_id
                    )
                 """
+        return self.pandas_query(query)
